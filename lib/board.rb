8cea1accacc3e7c4ba6e283f905f4744281d3d6b
Christopher Spagnesi <christopherspagnesi@gmail.com>
2020-10-26 16:01:52 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,3 @@
 # Define the variable board below.
-my_array= ["top left", "top middle", "top right", "middle left", "middle middle", "middle right", "bottom left", "bottom middle", "bottom right"]
+my_array = ["top left", "top middle", "top right", "middle left", "middle middle", "middle right", "bottom left", "bottom middle", "bottom right"]
 

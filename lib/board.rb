c1c52a2b63b4c1fd5c45ce3125b9674482ed0f99
Christopher Spagnesi <christopherspagnesi@gmail.com>
2020-10-26 16:05:27 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,5 @@
-# Define the variable board below.
-row = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
+# Define the variable board be
+board = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
 
 # Position 1: Left
 row (0)
